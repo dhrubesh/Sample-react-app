0eb5c944586891f1782c45185aaee1efa965c5bc
dhrubesh <dhrubesh97@gmail.com>
2023-07-28 15:44:55 +0530

    feat: add pagination to the player list

--- src/pages/CricketersPage/CricketersPage.tsx
+++ src/pages/CricketersPage/CricketersPage.tsx
@@ -1,5 +1,62 @@
+import { useEffect, useState } from 'react';
+import { CricketerList } from '@/components';
+import getPlayers, { TPlayer } from '@/API/get-players';
+import { CustomTablePagination } from './CricketersPage.Styled';
+import Paper from '@mui/material/Paper';
+
 const CricketersPage = () => {
-  return <>CricketersPage</>;
+  const [data, setData] = useState<TPlayer[]>([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  useEffect(() => {
+    getPlayers().then(setData);
+  }, []);
+
+  return (
+    <>
+      <CricketerList
+        players={
+          rowsPerPage > 0
+            ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : data
+        }
+      />
+      <Paper>
+        <CustomTablePagination
+          rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+          colSpan={3}
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          slotProps={{
+            select: {
+              'aria-label': 'rows per page',
+            },
+            actions: {
+              showFirstButton: true,
+              showLastButton: true,
+            },
+          }}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </>
+  );
 };
 
 export default CricketersPage;
